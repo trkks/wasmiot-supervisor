0f91b07486b53cccbcff3c574dfdaff38264cf5d
Juho Tarkkanen <juho.a.tarkkanen@jyu.fi>
2023-11-07 16:40:10 +0000

    Add supported datafile media types to the utils 'package'(?) root

---
+++ host_app/utils/__init__.py
@@ -0,0 +1,13 @@
+"""
+Common utilities.
+"""
+
+FILE_TYPES = [
+    "image/png",
+    "image/jpeg",
+    "image/jpg",
+    "application/octet-stream"
+]
+"""
+Media types of files that are supported in requests.
+"""
